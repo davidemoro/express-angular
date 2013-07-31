6841ce4261bfc6baf60254ce4f9f2c51084082c1
Davide Moro <davide.moro@gmail.com>
2013-07-31 21:38:08 +0200

    merged express app with angularjs/yeoman

--- app.js
+++ app.js
@@ -13,22 +13,25 @@ var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
+app.set('views', __dirname + '/app');
 app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
+  app.use(express.static(path.join(__dirname, 'app')));
   app.use(express.errorHandler());
+}
+else {
+  app.use(express.static(path.join(__dirname, 'dist')));
 }
 
 app.get('/', routes.index);
-app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
